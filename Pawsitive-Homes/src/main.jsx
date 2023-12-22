a4fe8e26e9cffaecd3ba9066d29cdfae929e7d19
Miguel Martins <mfproencamartins@gmail.com>
2023-12-22 15:24:54 +0000

    Dashboard and Page404 added

--- Pawsitive-Homes/src/main.jsx
+++ Pawsitive-Homes/src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { MantineProvider } from '@mantine/core';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>    
+    <BrowserRouter>
+      <MantineProvider>
+        <App />
+      </MantineProvider> 
+    </BrowserRouter>       
   </React.StrictMode>,
 )
---
+++ Pawsitive-Homes/src/pages/DashboardPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AnimalList from "../components/AnimalList";
+
+
+function DashboardPage() {
+    return (
+        <div> 
+            <h1>Pawsitive Homes</h1>
+            <AnimalList />
+        </div>
+    )
+
+}
+
+export default DashboardPage
