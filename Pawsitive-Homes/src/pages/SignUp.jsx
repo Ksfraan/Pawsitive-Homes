7e48b5bb480310c3100b2b78eedbbfec18ed948f
chuinga <mfproencamartins@gmail.com>
2024-01-06 16:55:16 +0000

    Signup styles and funcionalities added

--- Pawsitive-Homes/src/pages/SignUp.jsx
+++ Pawsitive-Homes/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import PageContainer from '../components/PageContainer';
-/* import react from '@vitejs/plugin-react-swc'; */
+import { useState } from 'react';
 import '../styles/Signup.css';
 import userIcon from '../assets/person.png';
 import emailIcon from '../assets/email.png';
@@ -7,35 +7,40 @@ import passwordIcon from '../assets/password.png';
 
 
 function SignUp() {
+
+    const [action, setAction] = useState('Create account');
+
     return (
         <PageContainer>
             <div className='container'>
                 <div className='top'>
-                    <div className='text'>Sign Up</div>
+                    <div className='text'>{action}</div>
                     <div className='underline'></div>
                 </div>
                 <div className='inputs'>
                     <div className="input">
-                        <img src={userIcon} alt="" />
+                        <img src={userIcon} alt="user" />
                         <input type="text" placeholder='Name' />
                     </div>
                     <div className="input">
-                        <img src={emailIcon} alt="" />
-                        <input type="email" placeholder='Enter your email' />
+                        <img src={emailIcon} alt="email" />
+                        <input type="email" placeholder='Email' />
                     </div>
                     <div className="input">
-                        <img src={passwordIcon} alt="" />
-                        <input type="password" placeholder='Choose password' />
+                        <img src={passwordIcon} alt="password" />
+                        <input type="password" placeholder='Create password' />
                     </div>
                     <div className="input">
-                        <img src={passwordIcon} alt="" />
+                        <img src={passwordIcon} alt="password" />
                         <input type="password" placeholder='Confirm password' />
                     </div>
                 </div> 
                 <div className="forgotPassword">Forgot password?</div>   
                 <div className="submitContainer">
-                    <div className="submit">Sign Up</div>
-                    <div className="submit">Sign In</div>
+                    <div className={action === 'Welcome Back!' ? 'submit gray' : 'submit'} 
+                    onClick={() => {setAction('Create account')}}>Sign Up</div>
+                    <div className={action === 'Create account' ? 'submit gray' : 'submit'} 
+                    onClick={() => {setAction('Welcome Back!')}}>Login</div>
                 </div>           
             </div>
         </PageContainer>
