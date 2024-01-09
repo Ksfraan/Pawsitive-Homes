920654479939d9f359e3a6cb0dfe92e35f0e6910
“Carina <ksfraan@hotmail.com>
2024-01-09 16:56:44 +0000

    remove nvmrc

--- Pawsitive-Homes/src/pages/AnimalList.jsx
+++ Pawsitive-Homes/src/pages/AnimalList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Link, useFetcher } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ButtonWithLink from '../components/ButtonWithLink';
 import { useContext, useState } from 'react';
 import PlusSign from '../assets/plus-sign.png';
@@ -31,55 +31,99 @@ const AnimalList = ({ animalSpecies }) => {
     const animalData = animals?.filter((animal) => {
         const speciesCondition =
             animalSpecies === 'All Species' ||
-            (animalSpecies === 'Others' && animal.species !== 'Cat' && animal.species !== 'Dog') ||
+            (animalSpecies === 'Others' &&
+                animal.species !== 'Cat' &&
+                animal.species !== 'Dog') ||
             animal.species === animalSpecies;
-  
+
         const ageCondition =
-            selectedAge === '' || 
-            (animal.age === selectedAge && typeof animal.age === 'string') || 
-            (animal.age == selectedAge && typeof animal.age === 'number'); 
-  
-        const genderCondition = !selectedGender || animal.gender === selectedGender;
+            selectedAge === '' ||
+            (animal.age === selectedAge && typeof animal.age === 'string') ||
+            (animal.age == selectedAge && typeof animal.age === 'number');
+
+        const genderCondition =
+            !selectedGender || animal.gender === selectedGender;
         const cityCondition = !selectedCity || animal.city === selectedCity;
-  
-        return speciesCondition && ageCondition && genderCondition && cityCondition;
+
+        return (
+            speciesCondition && ageCondition && genderCondition && cityCondition
+        );
     });
 
     return (
         <PageContainer>
             <div className='animals-list'>
                 <div className='search'>
-                <select name='Age' 
-                style={search === 'searchOff' ? {display: 'none'} : {display: 'block'}}
-                value={selectedAge}
-                onChange={(event) => setSelectedAge(event.target.value)}>
-                    <option value="">All ages</option>
-                    {sortArray([...new Set(animals.map((options) => options.age))]).map((age, i) => (
-                        <option key={i}>{age}</option>
-                    ))} 
-                </select>
-                <select name='Gender' 
-                style={search === 'searchOff' ? {display: 'none'} : {display: 'block'}}
-                value={selectedGender}
-                onChange={(event) => setSelectedGender(event.target.value)}>
-                    <option value="">All genders</option>
-                    {sortArray([...new Set(animals.map((options) => options.gender))]).map((gender, i) => (
-                        <option key={i}>{gender}</option>
-                    ))}
-                </select>
-                <select name='City' 
-                style={search === 'searchOff' ? {display: 'none'} : {display: 'block'}}
-                value={selectedCity}
-                onChange={(event) => setSelectedCity(event.target.value)}>
-                    <option value="">All cities</option>                    
-                    {sortArray([...new Set(animals.map((options) => options.city))]).map((city, i) => (
-                        <option key={i}>{city}</option>
-                    ))}
-                </select>
-                <img src={searchIcon} alt="Search" 
-                    onClick={search === 'searchOff' ? 
-                    () => { setSearch('searchOn') } : 
-                    () => { setSearch('searchOff') }}/>                    
+                    <select
+                        name='Age'
+                        style={
+                            search === 'searchOff'
+                                ? { display: 'none' }
+                                : { display: 'block' }
+                        }
+                        value={selectedAge}
+                        onChange={(event) => setSelectedAge(event.target.value)}
+                    >
+                        <option value=''>All ages</option>
+                        {sortArray([
+                            ...new Set(animals.map((options) => options.age)),
+                        ]).map((age, i) => (
+                            <option key={i}>{age}</option>
+                        ))}
+                    </select>
+                    <select
+                        name='Gender'
+                        style={
+                            search === 'searchOff'
+                                ? { display: 'none' }
+                                : { display: 'block' }
+                        }
+                        value={selectedGender}
+                        onChange={(event) =>
+                            setSelectedGender(event.target.value)
+                        }
+                    >
+                        <option value=''>All genders</option>
+                        {sortArray([
+                            ...new Set(
+                                animals.map((options) => options.gender)
+                            ),
+                        ]).map((gender, i) => (
+                            <option key={i}>{gender}</option>
+                        ))}
+                    </select>
+                    <select
+                        name='City'
+                        style={
+                            search === 'searchOff'
+                                ? { display: 'none' }
+                                : { display: 'block' }
+                        }
+                        value={selectedCity}
+                        onChange={(event) =>
+                            setSelectedCity(event.target.value)
+                        }
+                    >
+                        <option value=''>All cities</option>
+                        {sortArray([
+                            ...new Set(animals.map((options) => options.city)),
+                        ]).map((city, i) => (
+                            <option key={i}>{city}</option>
+                        ))}
+                    </select>
+                    <img
+                        src={searchIcon}
+                        alt='Search'
+                        onClick={
+                            search === 'searchOff'
+                                ? () => {
+                                      setSearch('searchOn');
+                                  }
+                                : () => {
+                                      setSearch('searchOff');
+                                  }
+                        }
+                    />
                 </div>
                 <center>
                     <h1>Animals for adoption</h1>
